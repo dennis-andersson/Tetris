49657904bb4015c3a74765510d6296a695f4951f
Dennis Andersson <dennis.andersson@gmail.com>
2021-03-24 09:50:02 +0100

    added code from Leila.

--- Sound.hpp
+++ Sound.hpp
@@ -2,6 +2,7 @@
 
 #include <SFML/Audio.hpp>
 #include <unordered_map>
+#include <fstream>
 
 enum SoundEffect
 {
@@ -17,6 +18,43 @@ private:
 public:
 	Sound() {}
 	~Sound() {}
+
+	void playSound()
+	{
+		std::ofstream out;
+
+		out.open("output.txt", std::ios::app);
+
+		//Load a sound buffer from a wav file
+		sf::SoundBuffer buffer;
+		if (!buffer.loadFromFile("sounds/pause.wav")) {
+			return;
+		}
+
+		//Display sound information
+		out << "orchestral.wav: " << std::endl;
+		out << " " << buffer.getDuration().asSeconds() << "seconds" << std::endl;
+		out << " " << buffer.getSampleRate() << "seconds / sec" << std::endl;
+		out << " " << buffer.getChannelCount() << "channels" << std::endl;
+
+		//Create a sound instance and play it
+		sf::Sound sound;
+		sound.setBuffer(buffer);
+		sound.play();
+
+		//Loop while the sound is playing
+		while (sound.getStatus() == sf::Sound::Playing)
+		{
+			//Leave some cpu time for other process
+			sf::sleep(sf::milliseconds(100));
+
+			//Display the playing position
+			out << "\rPlaying..." << sound.getPlayingOffset().asSeconds() << "sec  ";
+		}
+		out << std::endl << std::endl;
+
+		out.close();
+	}
 
 	void playSoundEffect(SoundEffect effect)
 	{
