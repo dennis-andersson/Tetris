db4bd6fb1777587362fff6934cbbe805fc37135f
Dennis Andersson <dennis.andersson@gmail.com>
2021-03-30 22:46:52 +0200

    added example code to CreditsScreen.

--- CreditsScreen.hpp
+++ CreditsScreen.hpp
@@ -4,15 +4,26 @@
 #include "Screen.hpp"
 #include "Game.hpp"
 #include "Background.hpp"
+#include "TextElement.hpp"
 
 class CreditsScreen : public Screen
 {
 private:
 	Background background;
+	TextElement test;
+	sf::Vector2f position;
+	int textSize{ 32 };
+	sf::Color textColor{ sf::Color::White };
+	sf::Uint8 textStyle{ sf::Text::Style::Regular };
 public:
 	CreditsScreen(sf::RenderWindow& window, Background& Background) : background(Background)
 	{
 		// Add whatever needs to be setup here.
+		position.x = window.getSize().x / 2;	// Center of the window
+		position.y = 100;
+
+		test.init("Test", position, textSize, textColor, textStyle);	// Set various properties
+		test.setOriginToCenter();				// It will be centered around its position
 	}
 
 	~CreditsScreen()
@@ -24,9 +35,11 @@ public:
 	// how it's normally implemented.
 	void render(sf::RenderWindow& window)
 	{
-		window.clear();
+		window.clear();							// Clear the window
 
 		// Draw stuff here.
+		background.draw(window);				// Draw background image
+		test.draw(window);						// Draw example text
 
 		window.display();
 	}
